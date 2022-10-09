083698c37babc53cb2e06a1b350a1ada77a7f091
Romeoscript <romeobourne211@gmail.com>
2022-10-09 16:01:30 +0100

    broser router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import { Routes, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       hope its working 
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} /> */}
+      </Routes>
     </div>
   );
 }
