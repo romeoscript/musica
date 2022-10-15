53cce6b52ea7073deb31388ce9b925db23944bd4
Romeoscript <romeobourne211@gmail.com>
2022-10-15 13:31:43 +0100

    newly

--- src/pages/player/Player.jsx
+++ src/pages/player/Player.jsx
@@ -1,5 +1,7 @@
+import { PlayCircle, MusicSquareAdd, Heart } from "iconsax-react";
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./Player.scss";
 
 const Player = (props) => {
   const topChart = props.released;
@@ -11,9 +13,43 @@ const Player = (props) => {
   const { url, title, image } = topPlayed;
   console.log(topPlayed);
   return (
-    <div className="shared__container">
+    <div className="shared__container player__container">
+      <div className="player__image-container">
+        <img src={image} alt="" />
+      </div>
+
+      <div className="music__tunes">
+        <div className="music__tunes-image">
+          <img src={image} alt="" />
+        </div>
+        <div className="music__tunes-text">
+          <section className="music__select">
+            <h2>Tomorrow’s tunes</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis
+            </p>
+            <p>64 songs ~ 16 hrs+</p>
+          </section>
+          <section className="music__tunes-play__section">
+            <div className="play">
+              {" "}
+              <PlayCircle size="20" color="#FACD66" variant="Bold" /> <p>Play all</p>
+            </div>
+            <div className="add__collection">
+              <MusicSquareAdd size="20" color="#FACD66" variant="Bold" />
+              <p>Add to collection</p>
+            </div>
+            <div className="heart__section">
+              <Heart size="20" color="#FACD66" variant="Bold" />
+            </div>
+          </section>
+        </div>
+      </div>
+      {/* music tunes ends here */}
+
       {url}
-      <img src={image} alt="" />
+
       {title}
       {params.musicId}
     </div>
